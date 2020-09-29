f37aea87badce5656b822e42a3d7a676d62355a0
ingvord <mail@ingvord.ru>
2020-09-29 10:10:25 +0200

    progress

--- axsis.py
+++ axsis.py
@@ -17,15 +17,15 @@ from pi_device import create_pi_device as internal_create_pi_device
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(PiController, '/controllers/<int:id>')
-api.add_resource(PiControllerServoMode, '/controllers/<int:id>/servo')
-api.add_resource(PiControllerReference, '/controllers/<int:id>/reference')
-api.add_resource(PiControllerPosition, '/controllers/<int:id>/position')
-api.add_resource(PiControllerHome, '/controllers/<int:id>/home')
-api.add_resource(PiControllerStop, '/controllers/<int:id>/stop')
-api.add_resource(PiAxis, '/controllers/<int:controller_id>/axis/<string:id>')
-api.add_resource(PiAxisReference, '/controllers/<int:controller_id>/axis/<string:id>/reference')
-api.add_resource(PiAxisPosition, '/controllers/<int:controller_id>/axis/<string:id>/position')
+api.add_resource(PiController, '/axsis/controllers/<int:id>')
+api.add_resource(PiControllerServoMode, '/axsis/controllers/<int:id>/servo')
+api.add_resource(PiControllerReference, '/axsis/controllers/<int:id>/reference')
+api.add_resource(PiControllerPosition, '/axsis/controllers/<int:id>/position')
+api.add_resource(PiControllerHome, '/axsis/controllers/<int:id>/home')
+api.add_resource(PiControllerStop, '/axsis/controllers/<int:id>/stop')
+api.add_resource(PiAxis, '/axsis/controllers/<int:controller_id>/axis/<string:id>')
+api.add_resource(PiAxisReference, '/axsis/controllers/<int:controller_id>/axis/<string:id>/reference')
+api.add_resource(PiAxisPosition, '/axsis/controllers/<int:controller_id>/axis/<string:id>/position')
 
 
 @app.before_request
@@ -34,4 +34,10 @@ def create_pi_device():
     port = request.args.get('port', default=50000)
 
     request.pi_device = internal_create_pi_device(host, port)
+
+
+@app.after_request
+def destroy_pi_device(response):
+    request.pi_device.CloseConnection()
+    return response
 
--- gcsdevice.py
+++ gcsdevice.py
@@ -5,6 +5,55 @@ Created on Thu Apr 16 13:32:08 2020
 @author: ingvord
 """
 
+version = {
+  "version": "libpi_pi_gcs2.so.3.9.8.1 \nPath: /usr/local/PI/pi_gcs_translator/PISTAGES3.DB Version: 3.0.17.0 \n2: (c)2017 Physik Instrumente (PI) GmbH & Co. KG, C-663.12C885, 018550039, 00.039 \n3: (c)2017 Physik Instrumente (PI) GmbH & Co. KG, C-663.12C885, 018550040, 00.039 \n4: (c)2017 Physik Instrumente (PI) GmbH & Co. KG, C-663.12C885, 018550041, 00.039 \n5: (c)2017 Physik Instrumente (PI) GmbH & Co. KG, C-663.12C885, 018550042, 00.039 \n6: (c)2017 Physik Instrumente (PI) GmbH & Co. KG, C-663.12C885, 018550043, 00.039 \n7: (c)2017 Physik Instrumente (PI) GmbH & Co. KG, C-663.12C885, 018550044, 00.039 \n8: (c)2017 Physik Instrumente (PI) GmbH & Co. KG, C-663.12C885, 018550046, 00.039 \n9: (c)2017 Physik Instrumente (PI) GmbH & Co. KG, C-663.12C885, 018550045, 00.039 \n10: (c)2017 Physik Instrumente (PI) GmbH & Co. KG, C-663.12C885, 018550047, 00.039 \n11: (c)2017 Physik Instrumente (PI) GmbH & Co. KG, C-663.12C885, 018550048, 00.039 \n12: (c)2017 Physik Instrumente (PI) GmbH & Co. KG, C-663.12C885, 018550049, 00.039 \n13: (c)2017 Physik Instrumente (PI) GmbH & Co. KG, C-663.12C885, 018550051, 00.039 \nFW_ARM: V1.0.0.1 \nPIPython: 1.3.9.40\n"
+}
+
+servo = {
+    "1": False,
+    "3": False,
+    "5": False,
+    "7": False,
+    "9": False,
+    "11": False,
+    "13": False,
+    "15": False,
+    "17": False,
+    "19": False,
+    "21": False,
+    "23": False
+}
+
+reference = {
+    "1": False,
+    "3": False,
+    "5": False,
+    "7": False,
+    "9": False,
+    "11": False,
+    "13": False,
+    "15": False,
+    "17": False,
+    "19": False,
+    "21": False,
+    "23": False
+}
+
+position = {
+    "1": 12.2000001,
+    "3": 12.2000001,
+    "5": 12.2000001,
+    "7": 12.2000001,
+    "9": 12.2000001,
+    "11": 12.2000001,
+    "13": 12.2000001,
+    "15": 12.2000001,
+    "17": 12.2000001,
+    "19": 12.2000001,
+    "21": 12.2000001,
+    "23": 12.2000001
+}
+
 
 class GCSDevice():
     def __init__(self):
@@ -13,6 +62,9 @@ class GCSDevice():
     def ConnectTCPIP(self, host, port):
         return None
 
+    def CloseConnection(self):
+        pass
+
     def IsConnected(self):
         return True
 
@@ -20,37 +72,22 @@ class GCSDevice():
         return True
 
     def qVER(self):
-        return "Some version"
+        return version
 
     def qSVO(self):
-        return {
-            '1': True,
-            '3': True,
-            '5': True
-        }
+        return servo
 
     def SVO(self, values):
         pass
 
-
     def qFRF(self, **kwargs):
-        return {
-            '1': True,
-            '3': True,
-            '5': True
-        }
-
+        return reference
 
     def FRF(self, values):
         pass
 
-
     def qPOS(self):
-        return {
-            '1': 12.2,
-            '3': 14,
-            '5': 8.3
-        }
+        return position
 
     def MOV(self, values):
         pass
