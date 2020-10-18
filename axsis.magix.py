4ad587b122948cd854439af5334cc47f01162ab8
ingvord <tla-ingvord@yandex.ru>
2020-10-18 13:08:21 +0300

    progress

--- axsis.magix.py
+++ axsis.magix.py
@@ -1,14 +1,91 @@
-import asyncio, rx, os, json
-from magix_client import MagixHttpClient
+import asyncio
+import json
+import os
+import time
+from functools import reduce
+
+import rx.operators as ops
+from magix_client import MagixHttpClient, Message
 from rx.scheduler.eventloop import AsyncIOScheduler
 
-magix_host = os.getenv('MAGIX_HOST', 'http://localhost:8080')
+from pi_device import create_pi_device
+
+kMagixHost = os.getenv('MAGIX_HOST', 'http://localhost:8080')
+kChannel = 'axsis-xes'
+
+
+class AxsisMessage:
+    def __init__(self, ip, action, value, port=50000):
+        self.ip = ip
+        self.port = port
+        self.action = action
+        self.value = value
+
+
+# TODO extract hierarchy
+class ActionExecuter:
+    def __init__(self, magix: MagixHttpClient, pi_device, msg: Message):
+        self.magix = magix
+        self.target = pi_device
+        self.message = msg
+        self.action = msg.payload
+
+    async def execute(self):
+        data = self.action.value
+        try:
+            self.target.MOV(data)
+            stopped = False
+            while not stopped:
+                await asyncio.sleep(0.1)
+
+                def create_message():
+                    pos = self.target.qPOS()
+                    return Message(id=time.time_ns(), parentId=self.message.id, target=self.message.origin,
+                                   origin='axsis', payload=AxsisMessage(ip=self.action.ip, action='qPOS', value=pos))
+
+                self.magix.broadcast(create_message(), channel=kChannel)
+                stopped = not reduce(lambda a, b: a or b, self.target.IsMoving(list(data.keys())).values())
+        finally:
+            self.target.CloseConnection()
+        pass
+
+
+class AxsisObserver:
+    def __init__(self, magix):
+        self.magix = magix
+
+    def on_next(self, msg):
+        try:
+            pi_device = create_pi_device(msg.payload.ip, msg.payload.port)
+            executer = ActionExecuter(self.magix, pi_device, msg)
+            asyncio.run(executer.execute())
+        except Exception as err:
+            msg = Message(id=time.time_ns(), origin='axsis', action='error', parentId=msg.id,
+                          payload={'error': 'Internal error has occurred: {0}.'.format(err)})
+            self.magix.broadcast(msg,
+                                 channel=kChannel)
+        pass
+
+    def on_error(self, err):
+        self.magix.broadcast(Message(id=time.time_ns(), origin='axsis', action='error',
+                                     payload={
+                                         'error': 'Critical error has occurred: {0}. Please restart AXSIS Magix connector'.format(
+                                             err)}),
+                             channel=kChannel)
+
+    def on_completed(self):
+        pass
 
 
 def main():
     loop = asyncio.get_event_loop()
-    client = MagixHttpClient(magix_host)
-    client.observe().subscribe(lambda event: print(json.loads(event.data)), scheduler=AsyncIOScheduler(loop))
+    client = MagixHttpClient(kMagixHost)
+    observer = AxsisObserver(client)
+    client.observe(channel=kChannel).pipe(
+        ops.filter(lambda event: json.loads(event.data).get('target') == 'axsis'),
+        ops.map(lambda event: Message.from_json(event.data, payload_cls=AxsisMessage))
+        # TODO proxy object or optimize somehow
+    ).subscribe(observer, scheduler=AsyncIOScheduler(loop))
     loop.run_forever()
     pass
 
