1cf411026638a8d0e5f6af22545b4b2c45bb0914
ingvord <igor.khokhriakov@rcsb.org>
2022-07-05 17:52:29 -0700

    Switch to apm agent

--- axsis.magix.py
+++ axsis.magix.py
@@ -96,7 +96,7 @@ def main():
     observer = AxsisObserver(client, loop)
     client.observe(channel=kChannel).pipe(
         ops.filter(lambda event: json.loads(event.data).get('target') == 'axsis'),
-        ops.do_action(lambda event: kApmClient.begin_transaction('magix', trace_parent=elasticapm.trace_parent_from_string(str(json.loads(event.data).get('traceparent'))))),
+        ops.do_action(lambda event: kApmClient.begin_transaction('magix', trace_parent=elasticapm.trace_parent_from_string(str(json.loads(event.data).get('id'))))),
         ops.map(lambda event: Message.from_json(event.data, payload_cls=AxsisMessage)),
         ops.do_action(lambda event: kApmClient.end_transaction('magix', 'success'))
         # TODO proxy object or optimize somehow
